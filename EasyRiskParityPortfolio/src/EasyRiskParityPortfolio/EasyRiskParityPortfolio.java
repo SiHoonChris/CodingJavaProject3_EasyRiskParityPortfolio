1bc643d3e3c726e7527d99966d3d1345f8534600
SiHoonChris <chrisleegpock@gmail.com>
2022-11-19 17:02:46 +0900

    11-19-2022

---
+++ EasyRiskParityPortfolio/src/EasyRiskParityPortfolio/EasyRiskParityPortfolio.java
@@ -0,0 +1,13 @@
+package EasyRiskParityPortfolio;
+
+// 1. 종목 정보 입력 : 종목명, TICKER, 연도별 수익률
+// 2. 종목에 대한 후속처리 : 연평균수익률, 조정 수익률, 표준편차 등 계산해서 각 테이블에 추가
+public class EasyRiskParityPortfolio {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+} // public class EasyRiskParityPortfolio{} - END
---
+++ EasyRiskParityPortfolio/src/EasyRiskParityPortfolio/dbConnector.java
@@ -0,0 +1,6 @@
+package EasyRiskParityPortfolio;
+
+// 1. MySQL 데이터베이스와 자바 연결
+public class dbConnector {
+
+}
